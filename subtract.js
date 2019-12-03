4909bcb37d62329526cd49fb7a7d023d85e69898
ni3kumar <sawantnitinkumar@gmail.com>
2019-12-03 15:39:23 +0530

    extra subtract comment

---
+++ subtract.js
@@ -0,0 +1,10 @@
+// subtract.js
+// return subtraction of two numbers
+function subtract(a, b, bMINUSa) {
+    if (bMINUSa === true) {
+        return b - a;
+    }
+    else {
+        return a - b;
+    }
+};
